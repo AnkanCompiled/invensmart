507fa700ede08f21b52b864a43ce3be35eae7cb3
AnkanCompiled <ankanb560@gmail.com>
2024-10-26 21:20:08 +0530

    revert

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,19 +1,16 @@
 import express from "express";
 import userRoute from "./userRoute.js";
 import productRoute from "./productRoute.js";
+
 import transactionsRoute from "./transactionRoute";
-import categoryRoute from "./categoryRouter.js"
-import supplierRoute from "./supplier.js"
 
 
-const indexRoute = express.Router();
+const Router = express.Router();
 
 
-indexRoute.use("/", userRoute);
-indexRoute.use("/products",productRoute);
-indexRoute.use("/transactions", transactionsRoute);
-indexRoute.use("/categories", categoryRoute);
-indexRoute.use("/supplier",supplierRoute);
+Router.use("/", userRoute);
+Router.use("/products", productRoute);
+Router.use("/transactions", transactionsRoute);
 
 
-export default indexRoute;
+export default Router;
--- server/routes/supplier.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import auth from "../middleware/auth.js";
-import supplierController from "../controller/supplierConrtoller.js"
-const supplierroute = express.Router();
-
-supplierroute.post("/",auth.authenticate,auth.authorization,supplierController.suppliers);
-
-
-export default supplierroute;

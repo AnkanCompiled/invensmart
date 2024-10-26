36ba4472c7f354de72679b49da54acd1f23cae6b
AnkanCompiled <ankanb560@gmail.com>
2024-10-26 16:29:40 +0530

    error connection

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import errorHandler from "./error/errorHandler.js";
-import routes from "./routes";
+import routes from "./routes/index.js";
 
 dotenv.config();
 

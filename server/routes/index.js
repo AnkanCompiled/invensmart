ef14989f0fbaff99d3fdad7bddc21eae42438492
Jeet Pal <jeetpal7804322@gmail.com>
2024-10-26 17:01:02 +0530

    Revert "Merge pull request #6 from AnkanCompiled/arif"
    This reverts commit cd82c740baf60d19e354fc501ee23b9125137aa4, reversing
    changes made to c462fc743c34ba4121064a518e909db53082df3e.

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,13 +2,13 @@ import express from "express";
 import userRoute from "./userRoute";
 import productRoute from "./productRoute.js";
 import transactionsRoute from "./transactionRoute";
-import categoryRoute from "./categoryRouter.js"
+
 
 const Router = express.Router();
 
 Router.use("/", userRoute);
 Router.use("/products",productRoute);
 Router.use("/transactions", transactionsRoute);
-Router.use("/categories", categoryRoute);
+
 
 export default Router;
